5085210d4b6654221abccfe7852c764de27c405d
hieuld2272002 <thuyettkls@gmail.com>
2022-10-11 08:37:35 +0700

    hieu up

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose =require('mongoose');
+var dotenv =require('dotenv');
+dotenv.config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("BD connection Successfull !"))
+    .catch((err) => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
