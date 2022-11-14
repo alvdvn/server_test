b5f57544c65bfd903d236cdc09e002fcb15e9083
Quân <quanminhfc@gmail.com>
2022-11-14 23:43:26 +0700

    push noti chuyen trang thai don hang

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,8 @@
 const orderModel = require("../models/order.model");
 const userModel = require("../models/user.model");
+var FCM = require('fcm-node');
+var serverKey = 'AAAA4oos57k:APA91bFyYJ2fZEP7jlGULUwC0NSc4VEFk86XTIx21ZX6f13LUbI1VyYGb0vS-7_ipjyi_-tOMMuk4PwtvveR_0fjixNC3ZcLEXbyGiQVoWO3VRX0xfUJknZ6Yico7YrhbBCA6oux6RTz';
+var fcm = new FCM(serverKey);
 
 exports.getFormlistOrder = async (req, res, next)=>{
     const listOrder = await orderModel.find();
@@ -21,16 +24,47 @@ const userItems =UserOderData.products.map( function (item) {
     res.render('./orders/detailOrder',{UserOderData:UserOderData});
 }
 //edit detail order
-exports.PostDetailOrder = (req,res)=>{
+exports.PostDetailOrder = async (req,res)=>{
     let dieu_kien ={
         _id: req.params.id
     }
+
+
     let du_lieu ={
         status :req.body.status
     }
+    //id user
     orderModel.updateOne(dieu_kien,du_lieu,function (err,res){
         if (err){
             console.log("Loi update"+err.message,{msg:'Lỗi update'})
+        }
+    });
+    const getIdOrder = await orderModel.findById({_id:req.params.id});
+    let id = getIdOrder.userId;
+    let idOrder = getIdOrder._id;
+    let trangthai = getIdOrder.status;
+
+    var message = {
+        to:"/topics/"+id,
+        collapse_key: 'your_collapse_key',
+
+        notification: {
+            title: "Trạng thái đơn hàng",
+            body: "Đơn hàng id: "+idOrder+" đã chuyển thành "+trangthai,
+            image: "https://blog.abit.vn/wp-content/uploads/2020/12/giao-hang-lazada-3-compressed.jpg"
+        },
+
+        data: {
+            my_key: 'my value',
+            my_another_key: 'my another value'
+        }
+    };
+    console.log(message)
+    fcm.send(message, function(err, response){
+        if (err) {
+            console.log("Something has gone wrong!", err);
+        } else {
+            console.log("Successfully sent with response: ", response);
         }
     });
     res.redirect('/order/listorder');
