5fa0a3639c0f5ec44f78dcdab1d411cedf86c34d
hieuld2272002 <thuyettkls@gmail.com>
2022-11-17 11:27:51 +0700

    hieu 16/11

--- controllers/thongke.controller.js
+++ controllers/thongke.controller.js
@@ -8,7 +8,7 @@ exports.getFilter = async (req,res)=>{
  //get start of the day
     const today = moment().startOf('day')
   const findOrderbyday = await OrderModel.find({
-      CreatedAt:{
+      createdAt:{
           $gte: today.toDate(),
           $lt:moment(today).endOf('day').toDate()
       }
@@ -29,6 +29,34 @@ exports.getFilter = async (req,res)=>{
     }
 }
 
+exports.getFilterMonth = async (req,res)=>{
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth()-1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth()-1));
+
+    try {
+        const income =await OrderModel.aggregate([
+            {$match:{createdAt:{$gte:previousMonth}}},
+            {
+                $project:{
+                    month: { $month: "$createdAt" },
+                    sales:"$Total",
+                },
+            },
+            {
+                $group:{
+                    _id:"$month",
+                    total:{$sum:"$sales"},
+                },
+            },
+        ]);
+        console.log(income);
+        res.status(200).json(income);
+    }catch (err){
+        return res.status(500).json({message:err.message})
+    }
+}
+
 exports.getFilterMonthtoYear =(req,res)=>{
     var months = ["jan", "feb", "mar", "apr", "may", "jun", "july", "aug", "sep", "oct", "nov", "dec"];
 
@@ -40,4 +68,60 @@ exports.getFilterMonthtoYear =(req,res)=>{
     console.log(months[month]);
 
     console.log(year);
+}
+
+exports.getfilterStatusOrder =async (req,res)=>{
+    let AllPendingOrder =0;
+    let AllConfirmedOrder =0;
+    let AllDeliverOrder =0;
+    let AllSuccessOrder =0;
+    let AllOrder =0;
+    const FindAllOrder= await OrderModel.find();
+    if (FindAllOrder.length ==null){
+        return res.status(404).json({message:"không tìm thấy bản ghi nào"});
+    }else {
+        for(let i =0;i<FindAllOrder.length;i++){
+            AllOrder++
+            if (FindAllOrder[i].status ==='Đang chờ xác nhận'){
+                AllPendingOrder++
+            }else if (FindAllOrder[i].status ==="Đang chuẩn bị hàng"){
+                AllConfirmedOrder++
+            }else if (FindAllOrder[i].status ==='Đang giao hàng'){
+                AllDeliverOrder++
+            }
+            else if (FindAllOrder[i].status ==='Giao hàng thành công'){
+                AllSuccessOrder++
+            }
+        }
+        res.status(200).json({
+            AllOrder:AllOrder,
+            pending:AllPendingOrder,
+            Confirmed:AllConfirmedOrder,
+            Deliver:AllDeliverOrder,
+            Success:AllSuccessOrder
+        });
+    }
+
+}
+
+exports.getfilterIncome =async (req,res)=>{
+    let Allincome =0;
+    try {
+        const getAllOrder = await OrderModel.find();
+
+        if (getAllOrder.length ==null){
+            return res.status.json({message:"không tìm thấy bản ghi nào"});
+        }else {
+            for (let i=0;i<getAllOrder.length;i++){
+                Allincome += getAllOrder[i].Total;
+            }
+        }
+        res.status(200).json({
+            Income:Allincome
+        });
+    }catch (err){
+        return res.status(500).json({
+            message:err.message
+        })
+    }
 }
--- routes/thongke.js
+++ routes/thongke.js
@@ -3,7 +3,18 @@ var router = express.Router();
 const thongKeController = require('../controllers/thongke.controller');
 
 router.get('/',thongKeController.getFormAdd);
+//get day
 router.get('/filterAmountByDay',thongKeController.getFilter);
+//get month
+router.get('/filterAmountByMonth',thongKeController.getFilterMonth);
+//get year (not done)
 router.get('/filterAmountMonthtoYear',thongKeController.getFilterMonthtoYear);
+//get all status order
+router.get('/filterPendingOrder',thongKeController.getfilterStatusOrder);
+//get all income order
+router.get('/filterIncome',thongKeController.getfilterIncome);
+
+
+
 
 module.exports = router;
