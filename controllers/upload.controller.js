2f31593c6627632bad023d9362395cc6c09f2ed0
hieuld2272002 <thuyettkls@gmail.com>
2022-11-30 00:41:54 +0700

    hieu add api 30/11

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -15,42 +15,35 @@ exports.getFormUpload = async (req,res,next)=>{
     }
     res.render('./products/upload',{Book:Book})
 }
-exports.postUpload= async (req,res,next)=>{
-    let condition = {_id: req.params.id};
-    const  book = await  productModel.findById(req.params.id).exec()
-        .catch(err=>{
-            console.log(err);
-        });
-    if (book == null){
-        return log("Comics not found");
-    }
-    console.log("anh: "+req.files)
-    if (req.files==""){
-        return res.render('./products/upload',{msg:'Chưa chọn file upload '});
-    }
-    const uploader = async (path)=> await cloudinary.uploads(path,'Products');
-    const urls =[]
-    const files =req.files
-    for (const file of files){
-        const {path} =file
-        const newPath =await uploader(path)
-        urls.push(newPath)
-        fs.unlinkSync(path)}
-    const filename = urls.map(function (item) {
-        return item.url;
-    })
+exports.postUpload= async (req,res)=>{
+  try {
+      let condition = {_id: req.params.id};
+      const uploader = async (path)=> await cloudinary.uploads(path);
+      const urls =[]
+      const files =req.files
+      for (const file of files){
+          const {path} =file
+          const newPath =await uploader(path)
+          urls.push(newPath)
+          fs.unlinkSync(path)}
+      const filename = urls.map(function (item) {
+          return item.url;
+      })
 
-    let obj ={
-        img_product:filename,
-    }
-   await productModel.updateOne(condition,obj).then(function (err,result){
-        if (err){
-            // return res.status(500).render('./products/upload',{msg:' da xay ra loi'})
-            console.log(err)
-        }else {
-            return res.status(200).render('./products/upload',{msg:' đã thành công'})
-        }
-    })
+      let obj ={
+          img_product:filename,
+      }
+      await productModel.updateOne(condition,obj).then(function (err,result){
+          if (err){
+               return res.status(500).render('./products/upload',{msg:' da xay ra loi'})
+              console.log(err)
+          }else {
+              return res.status(200).render('./products/upload',{msg:' đã thành công'})
+          }
+      })
 
-    return res.redirect('/pro/list');
+      return res.redirect('/pro/list');
+  }catch (e){
+      console.log(e.message);
+  }
 }
