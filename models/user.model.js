d100ce1fa89461c37581cd68f0123de729aa02eb
hieuld2272002 <thuyettkls@gmail.com>
2022-11-30 11:52:01 +0700

    hieu fix bug 30/11

--- models/user.model.js
+++ models/user.model.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema(
         password: { type: String, required: true },
         full_name: { type: String, required: true },
         address: { type: String},
-        phone_number: { type: Number,required:true },
+        phone_number: { type: String,required:true },
         role:{type:String,required:true, default:'User'},
         avatar:{type:String},
         tokens:[{
